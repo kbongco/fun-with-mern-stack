bf30dbf2fe4cb7f5d0b271cedcc4441e61c7d5e5
Kathleen Bongco <kathleen.bongco@gmail.com>
2020-04-22 17:53:48 -0400

    commit

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,9 @@ connection.once('open', () => {
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
 
-app.use('/exercises', exercisesRouter);
+
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
+
